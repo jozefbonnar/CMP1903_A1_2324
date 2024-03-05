73981c2922955c77e00967682de9f315e6b483ab
Jozef Bonnar <jozefbonnar@gmail.com>
2024-03-05 12:59:40 +0000

    Changed the TestDieRoll method so it rolls the die multiple times

--- CMP1903_A1_2324/Testing.cs
+++ CMP1903_A1_2324/Testing.cs
@@ -28,6 +28,7 @@ namespace CMP1903_A1_2324
             TestDieRoll();
             TestGameRollDice();
             TestStatistics();
+            Console.Write("Test complete. Press enter to exit.");
             Console.ReadLine();
         }
 
@@ -38,17 +39,15 @@ namespace CMP1903_A1_2324
         {
             Console.WriteLine("Testing the Die class");
             Die die = new Die();
-            int result = die.RollDie();
-            Debug.Assert(result >= 1 && result <= 6, "The result of the die roll is not between 1 and 6");
-            Console.WriteLine("The result of the die roll is: " + result);
-            if (result >= 1 && result <= 6)
+            int[] results = new int[50]; // Roll the die 50 times
+            Console.Write("Rolling the dice 50 times: ");
+            for (int i = 0; i < results.Length; i++)
             {
-                Console.WriteLine("Test Passed: The result of the die roll is between 1 and 6");
+                results[i] = die.RollDie();
+                Debug.Assert(results[i] >= 1 && results[i] <= 6, "The result of the die roll is not between 1 and 6");
+                Console.Write(results[i]+", ");
             }
-            else
-            {
-                Console.WriteLine("Test Failed: The result of the die roll is not between 1 and 6");
-            }
+            Console.WriteLine("\nTest Passed: The results of the die rolls are between 1 and 6");
         }
 
         /// <summary>
